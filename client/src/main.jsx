ca7d6f00e97c5c6be7f45dd8978a4eabf4832f4d
tiagosousa <tiagosousa.tams@hotmail.com>
2025-05-25 15:13:48 +0100

    creating the navbar with clerk login

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -13,7 +13,7 @@ if (!PUBLISHABLE_KEY) {
 }
 
 createRoot(document.getElementById('root')).render(
-  <ClerkProvider>
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
     <BrowserRouter>
       <AppContextProvider> 
         <App />
