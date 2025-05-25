b8e76e032ea6797e086195fe23d6bc8db9cb6656
tiagosousa <tiagosousa.tams@hotmail.com>
2025-05-25 11:42:21 +0100

    create the first routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Applications from './pages/Applications'
+import ApplyJob from './pages/ApplyJob'
 
 const App = () => {
   return (
-    <div className='text-3xl '>App</div>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/apply-job/:id' element={<ApplyJob />} />
+        <Route path='/applications' element={<Applications />} />
+      </Routes>
+    </div>
   )
 }
 
