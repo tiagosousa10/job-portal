0f8d3193eb5e57be7fbde742638fb20f339c883a
tiagosousa <tiagosousa.tams@hotmail.com>
2025-05-26 19:38:44 +0100

    starting the jobCard

---
+++ client/src/components/JobCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const JobCard = ({job}) => {
+  return (
+    <div>
+      <div>
+         <img src={} alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default JobCard
--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -53,7 +53,7 @@ const JobListing = () => {
 
           {/* location filter */}
          <div className={'max-lg:hidden '}>
-            <h4 className='font-medium text-lg py-4'>Search by Location</h4>
+            <h4 className='font-medium text-lg py-4 pt-14'>Search by Location</h4>
             <ul className='space-y-4 text-gray-600'>
                {
                   JobLocations.map((location, index) => (
@@ -68,6 +68,13 @@ const JobListing = () => {
       </div>
 
       {/* job listings */}
+      <section className='w-full lg:w-3/4 text-gray-800 max-lg:px-4'>
+         <h3 className='font-medium text-3xl py-2' id="job-list">Latest jobs</h3>
+         <p className='mb-8'>Get your desired job from top companies</p>
+         <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4'>
+
+         </div>
+      </section>
     </div>
   )
 }
