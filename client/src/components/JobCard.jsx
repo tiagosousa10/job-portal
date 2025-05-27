6b991dd42e9979971ef062a62aa04788f0e46bd9
tiagosousa <tiagosousa.tams@hotmail.com>
2025-05-27 16:29:22 +0100

    fetching dummy data for the jobcard

--- client/src/components/JobCard.jsx
+++ client/src/components/JobCard.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const JobCard = ({job}) => {
   return (
     <div>
       <div>
-         <img src={} alt="" />
+         <img src={assets.company_icon} alt="" />
+      </div>
+      <h4>{job.title}</h4>
+      <div>
+        <span>{job.location}</span>
+        <span>{job.level}</span>
+      </div>
+      <p dangerouslySetInnerHTML={{__html: job.description.slice(0,150)}}></p>
+      <div>
+        <button>Apply Now</button>
+        <button>Learn More</button>
       </div>
     </div>
   )
