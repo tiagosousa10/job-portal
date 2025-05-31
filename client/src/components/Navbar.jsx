892d2542497a7843ce2819f897516ccd7c29fb2e
tiagosousa <tiagosousa.tams@hotmail.com>
2025-05-31 19:14:38 +0100

    right section to find more jobs added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,43 +1,45 @@
-import React from 'react'
-import {assets} from '../assets/assets'
-import {useClerk, UserButton, useUser} from '@clerk/clerk-react'
-import { Link } from 'react-router-dom'
-
-
-
+import React from "react";
+import { assets } from "../assets/assets";
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-  const {openSignIn} = useClerk() // to open the sign in modal
-  const {user} = useUser() // to get the user
-
+  const { openSignIn } = useClerk(); // to open the sign in modal
+  const { user } = useUser(); // to get the user
+  const navigate = useNavigate();
 
   return (
-    <div className='shadow py-4'>
-      <div className='container px-4 2xl:px-20 mx-auto flex justify-between items-center'>
-         <img src={assets.logo} alt="" />
-         {user ? (
-          <div className='flex items-center gap-3'>
-            <Link to={"/applications"}>
-              Applied Jobs
-            </Link>
-            <p>
-              |
+    <div className="shadow py-4">
+      <div className="container px-4 2xl:px-20 mx-auto flex justify-between items-center">
+        <img
+          src={assets.logo}
+          alt=""
+          onClick={() => navigate("/")}
+          className="cursor-pointer"
+        />
+        {user ? (
+          <div className="flex items-center gap-3">
+            <Link to={"/applications"}>Applied Jobs</Link>
+            <p>|</p>
+            <p className="max-sm:hidden">
+              Hi, {user.firstName + " " + user.lastName}{" "}
             </p>
-            <p className='max-sm:hidden'>Hi,  {user.firstName + " " + user.lastName} </p>
             <UserButton />
           </div>
-         ) :(
-           <div className='flex gap-4 max-sm:text-xs'>
-            <button className='text-gray-600'>
-               Recruiter Login
+        ) : (
+          <div className="flex gap-4 max-sm:text-xs">
+            <button className="text-gray-600">Recruiter Login</button>
+            <button
+              onClick={(e) => openSignIn()}
+              className="bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full "
+            >
+              Login
             </button>
-            <button onClick={(e) => openSignIn()} className='bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full '>Login</button>
-         </div>
-         )}
-        
+          </div>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -6,6 +6,7 @@ import Navbar from "../components/Navbar";
 import { assets } from "../assets/assets";
 import kConvert from "k-convert";
 import moment from "moment";
+import JobCard from "../components/JobCard";
 
 const ApplyJob = () => {
   const { id } = useParams();
@@ -86,6 +87,21 @@ const ApplyJob = () => {
                 <button className="bg-blue-600 p-2.5 px-10 text-white rounded mt-10">
                   Apply Now
                 </button>
+              </div>
+              {/* Right Section More Jobs */}
+              <div className="w-full lg:w-1/3 mt-8 lg:mt-0 lg:ml-8 space-y-5">
+                <h2>More Jobs from {jobData.companyId.name}</h2>
+                {jobs
+                  .filter(
+                    (job) =>
+                      job._id !== jobData._id &&
+                      job.companyId._id === jobData.companyId._id
+                  )
+                  .filter((job) => true)
+                  .slice(0, 4)
+                  .map((job, index) => (
+                    <JobCard key={index} job={job} />
+                  ))}
               </div>
             </div>
           </div>
