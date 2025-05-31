603eb1aa426f8d19b0598a183c2134bc494749bc
tiagosousa <tiagosousa.tams@hotmail.com>
2025-05-31 18:36:37 +0100

    first jsx to the first component for apply jobs page

--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -5,6 +5,7 @@ import Loading from "../components/Loading";
 import Navbar from "../components/Navbar";
 import { assets } from "../assets/assets";
 import kConvert from "k-convert";
+import moment from "moment";
 
 const ApplyJob = () => {
   const { id } = useParams();
@@ -31,32 +32,47 @@ const ApplyJob = () => {
     <div>
       <>
         <Navbar />
-        <div>
-          <div>
-            <div>
-              <div>
-                <img src={jobData.companyId.image} alt="" />
-                <div>
-                  <h1>{jobData.title}</h1>
-                  <div>
-                    <span>
+        <div className="min-h-screen flex flex-col py-10 container px-4 2xl:px-20 mx-auto">
+          <div className="bg-white text-black rounded-lg w-full">
+            <div className="flex justify-center md:justify-between flex-wrap gap-8 px-14 py-20 mb-6 bg-sky-50 border border-sky-400 rounded-xl">
+              <div className="flex flex-col md:flex-row items-center">
+                <img
+                  className="h-24 bg-white rounded-lg p-4 mr-4 max-md:mb-4 border"
+                  src={jobData.companyId.image}
+                  alt=""
+                />
+                <div className="text-center md:text-left text-neutral-700">
+                  <h1 className="text-2xl sm:text-4xl font-medium">
+                    {jobData.title}
+                  </h1>
+                  <div className="flex flex-row flex-wrap max-md:justify-center gap-y-2 gap-6 items-center text-gray-600 mt-2">
+                    <span className="flex items-center gap-1">
                       <img src={assets.suitcase_icon} alt="" />
                       {jobData.companyId.name}
                     </span>
-                    <span>
+                    <span className="flex items-center gap-1">
                       <img src={assets.location_icon} alt="" />
                       {jobData.location}
                     </span>
-                    <span>
+                    <span className="flex items-center gap-1">
                       <img src={assets.person_icon} alt="" />
                       {jobData.level}
                     </span>
-                    <span>
+                    <span className="flex items-center gap-1">
                       <img src={assets.money_icon} alt="" />
                       CTC: {kConvert.convertTo(jobData.salary)}
                     </span>
                   </div>
                 </div>
+              </div>
+
+              <div className="flex flex-col justify-center text-end text-sm max-md:mx-auto max-md:text-center">
+                <button className="bg-blue-600 p-2.5 px-10 text-white rounded">
+                  Apply Now
+                </button>
+                <p className="mt-1 text-gray-600">
+                  Posted {moment(jobData.date).fromNow()}
+                </p>
               </div>
             </div>
           </div>
