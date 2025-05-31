41a24fe7dacfe932cb7e9879d5be102011b7ab5a
tiagosousa <tiagosousa.tams@hotmail.com>
2025-05-31 18:17:16 +0100

    create Loading component

--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { AppContext } from "../context/AppContext";
+import Loading from "../components/Loading";
 
 const ApplyJob = () => {
   const { id } = useParams();
@@ -23,7 +24,7 @@ const ApplyJob = () => {
     }
   }, [id, jobs]);
 
-  return <div></div>;
+  return jobData ? <div></div> : <Loading />;
 };
 
 export default ApplyJob;
