434a851bf09ff4f2b556a3ba087d8a977b0f93fb
tiagosousa <tiagosousa.tams@hotmail.com>
2025-06-01 16:02:56 +0100

    jobs applied table created

--- client/src/pages/Applications.jsx
+++ client/src/pages/Applications.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import { assets, jobsApplied } from "../assets/assets";
+import moment from "moment";
 const Applications = () => {
-  const [isEdited, setIsEdited] = React.useState(false);
+  const [isEdit, setIsEdit] = React.useState(false);
   const [resume, setResume] = React.useState(null);
 
   return (
@@ -10,12 +12,28 @@ const Applications = () => {
       <div className="container px-4 min-h-[65vh] 2xl:px-20 mx-auto my-10">
         <h2 className="text-2xl font-semibold ">Your Resume</h2>
         <div className="flex gap-2 mb-6 mt-3">
-          {isEdited ? (
+          {isEdit ? (
             <>
-              <label htmlFor="">
-                <p>Select Resume</p>
-                <input type="text" accept="application/pdf" />
+              <label className="flex items-center" htmlFor="resumeUpload">
+                <p className="bg-blue-100 text-blue-600 px-4 py-2 rounded-lg mr-2">
+                  Select Resume
+                </p>
+                <input
+                  id="resumeUpload"
+                  onChange={(e) => setResume(e.target.files[0])}
+                  type="text"
+                  accept="application/pdf"
+                  type="file"
+                  hidden
+                />
+                <img src={assets.profile_upload_icon} alt="" />
               </label>
+              <button
+                onClick={(e) => setIsEdit(false)}
+                className="bg-green-100 border border-green-400 rounded-lg px-4 py-2"
+              >
+                Save
+              </button>
             </>
           ) : (
             <div className="flex gap-2">
@@ -26,7 +44,7 @@ const Applications = () => {
                 Resume
               </a>
               <button
-                onClick={() => setIsEdited(true)}
+                onClick={() => setIsEdit(true)}
                 className="text-gray-500 border border-gray-300 rounded-lg px-4 py-2"
               >
                 Edit
@@ -34,6 +52,49 @@ const Applications = () => {
             </div>
           )}
         </div>
+        <h2 className="text-xl font-semibold mb-4">Jobs Applied</h2>
+        <table className="min-w-full bg-white border rounded-lg">
+          <thead>
+            <tr>
+              <th className="py-3 px-4 border-b text-left">Company</th>
+              <th className="py-3 px-4 border-b text-left">Job Title</th>
+              <th className="py-3 px-4 border-b text-left max-sm:hidden">
+                Location
+              </th>
+              <th className="py-3 px-4 border-b text-left max-sm:hidden">
+                Date
+              </th>
+              <th className="py-3 px-4 border-b text-left">Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {jobsApplied.map((job, index) =>
+              true ? (
+                <tr>
+                  <td className="py-3 px-4 flex items-center gap-2 border-b">
+                    <img className="w-8 h-8" src={job.logo} alt="" />
+                    {job.companyName}
+                  </td>
+                  <td className="py-2 px-4 border-b max-sm:hidden">
+                    {job.title}
+                  </td>
+                  <td className="py-2 px-4 border-b max-sm:hidden">
+                    {job.location}
+                  </td>
+                  <td className="py-2 px-4 border-b">
+                    {moment(job.date).format("ll")}
+                  </td>
+                  <td className="py-2 px-4 border-b">
+                      <span className={`${job.status === "Accepted" ? "bg-green-100" : job.status === "Rejected" ? "bg-red-100" : "bg-blue-100"} px-4 py-1.5 rounded`}>
+                    {job.status}
+
+                      </span>
+                    </td>
+                </tr>
+              ) : null
+            )}
+          </tbody>
+        </table>
       </div>
     </>
   );
