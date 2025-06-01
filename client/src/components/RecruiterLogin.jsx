a74ab8788b88bc4d0c302dbf8f41eb1585aa28c3
tiagosousa <tiagosousa.tams@hotmail.com>
2025-06-01 16:38:30 +0100

    modal for recruiter login updated

--- client/src/components/RecruiterLogin.jsx
+++ client/src/components/RecruiterLogin.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import { assets } from "../assets/assets";
+import { AppContext } from "../context/AppContext";
 
 const RecruiterLogin = () => {
   const [state, setState] = React.useState("Login");
@@ -11,15 +12,53 @@ const RecruiterLogin = () => {
 
   const [isTextDataSubmitted, setIsTextDataSubmitted] = React.useState(false);
 
+  const { setShowRecruiterLogin } = useContext(AppContext);
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+
+    if (state === "Sign Up" && !isTextDataSubmitted) {
+      setIsTextDataSubmitted(true);
+    }
+  };
+
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+
+    return () => {
+      document.body.style.overflow = "unset"; // Reset overflow when component unmounts
+    };
+  }, []);
+
   return (
     <div className="absolute top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center ">
-      <form className="relative bg-white p-10 rounded-xl text-slate-500">
+      <form
+        onSubmit={onSubmitHandler}
+        className="relative bg-white p-10 rounded-xl text-slate-500"
+      >
         <h1 className="text-center text-2xl text-neutral-700 font-medium">
           Recruiter {state}
         </h1>
         <p className="text-sm">Welcome back! Please sign in to continue</p>
         {state === "Sign Up" && isTextDataSubmitted ? (
-          <></>
+          <>
+            <div className="flex items-center gap-4 my-10">
+              <label htmlFor="image">
+                <img
+                  className="w-16 rounded-full"
+                  src={image ? URL.createObjectURL(image) : assets.upload_area}
+                  alt=""
+                />
+                <input
+                  onChange={(e) => setImage(e.target.files[0])}
+                  type="file"
+                  hidden
+                  id="image"
+                />
+              </label>
+              <p>Upload Company Logo</p>
+            </div>
+          </>
         ) : (
           <>
             {state !== "Login" && (
@@ -62,12 +101,21 @@ const RecruiterLogin = () => {
           </>
         )}
 
-        <p className="text-sm text-blue-600 my-4 cursor-pointer">
-          Forgot Password?
-        </p>
+        {state === "Login" && (
+          <p className="text-sm text-blue-600 my-4 cursor-pointer">
+            Forgot Password?
+          </p>
+        )}
 
-        <button className="bg-blue-600 w-full text-white py-2 rounded-full">
-          {state === "Login" ? "Login" : "Sign Up"}
+        <button
+          type="submit"
+          className="bg-blue-600 w-full text-white py-2 rounded-full mt-4"
+        >
+          {state === "Login"
+            ? "Login"
+            : isTextDataSubmitted
+            ? "Sign Up"
+            : "Next"}
         </button>
         {state === "Login" ? (
           <p className="mt-5 text-center">
@@ -90,6 +138,13 @@ const RecruiterLogin = () => {
             </span>
           </p>
         )}
+
+        <img
+          className="absolute top-5 right-5 cursor-pointer"
+          src={assets.cross_icon}
+          onClick={() => setShowRecruiterLogin(false)}
+          alt=""
+        />
       </form>
     </div>
   );
