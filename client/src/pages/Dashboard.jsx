742177752396124ae64c8c7a2a9f78059a199cc3
tiagosousa <tiagosousa.tams@hotmail.com>
2025-06-02 10:53:44 +0100

    sidebar added

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Outlet, useNavigate } from "react-router-dom";
+import { NavLink, Outlet, useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets";
 
 const Dashboard = () => {
@@ -31,6 +31,53 @@ const Dashboard = () => {
               </div>
             </div>
           </div>
+        </div>
+      </div>
+
+      <div className="flex items-start">
+        {/* Left Sidebar (Recruiter Panel) */}
+        <div className="inline-block min-h-screen border-r-2">
+          <ul className="flex flex-col items-start pt-5 text-gray-800">
+            <NavLink
+              className={({ isActive }) =>
+                `flex items-center p-3 sm:px-6 gap-2 w-full hover:bg-gray-100 ${
+                  isActive && "bg-blue-100 border-r-4 border-blue-500"
+                }`
+              }
+              to={"/dashboard/add-job"}
+            >
+              <img className="min-w-4" src={assets.add_icon} alt="" />
+              <p className="max-sm:hidden">Add Job</p>
+            </NavLink>
+
+            <NavLink
+              className={({ isActive }) =>
+                `flex items-center p-3 sm:px-6 gap-2 w-full hover:bg-gray-100 ${
+                  isActive && "bg-blue-100 border-r-4 border-blue-500"
+                }`
+              }
+              to={"/dashboard/manage-jobs"}
+            >
+              <img src={assets.home_icon} className="min-w-4" alt="" />
+              <p className="max-sm:hidden">Manage Jobs</p>
+            </NavLink>
+
+            <NavLink
+              className={({ isActive }) =>
+                `flex items-center p-3 sm:px-6 gap-2 w-full hover:bg-gray-100 ${
+                  isActive && "bg-blue-100 border-r-4 border-blue-500"
+                }`
+              }
+              to={"/dashboard/view-applications"}
+            >
+              <img src={assets.person_tick_icon} className="min-w-4" alt="" />
+              <p className="max-sm:hidden">View Applications</p>
+            </NavLink>
+          </ul>
+        </div>
+
+        <div>
+          <Outlet />
         </div>
       </div>
     </div>
