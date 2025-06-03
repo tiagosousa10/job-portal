c572a09ad9809cdb28440f6689fa60a3aed146b5
tiagosousa <tiagosousa.tams@hotmail.com>
2025-06-03 16:16:33 +0100

    create table with dummy data for the view applications page

--- client/src/pages/ViewApplications.jsx
+++ client/src/pages/ViewApplications.jsx
@@ -1,7 +1,52 @@
 import React from "react";
+import { assets, viewApplicationsPageData } from "../assets/assets";
 
 const ViewApplications = () => {
-  return <div>ViewApplications</div>;
+  return (
+    <div>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>User name</th>
+              <th>Job title</th>
+              <th>Location</th>
+              <th>Resume</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {viewApplicationsPageData.map((applicant, index) => (
+              <tr>
+                <td>{index + 1}</td>
+                <td>
+                  <img src={applicant.imgSrc} alt="" />
+                  <span>{applicant.name}</span>
+                </td>
+                <td>{applicant.jobTitle}</td>
+                <td>{applicant.location}</td>
+                <td>
+                  <a href="" target="_blank">
+                    Resume <img src={assets.resume_download_icon} alt="" />
+                  </a>
+                </td>
+                <td>
+                  <div>
+                    <button>...</button>
+                    <div>
+                      <button>Accept</button>
+                      <button>Reject</button>
+                    </div>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default ViewApplications;
