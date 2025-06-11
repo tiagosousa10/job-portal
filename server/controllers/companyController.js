f72b90413317e2aa4c402c7cec9fa81fc31d76c9
tiagosousa <tiagosousa.tams@hotmail.com>
2025-06-11 14:46:13 +0100

    create the first steps to add functionalities to companyController

---
+++ server/controllers/companyController.js
@@ -0,0 +1,76 @@
+//register a new company
+export const registerCompany = async ( req,res) => {
+  try {
+
+  } catch(error) {
+
+  }
+}
+
+//company login
+export const loginCompany = async ( req,res) => {
+  try {
+
+  } catch(error) {
+
+  }
+}
+
+//get company details
+export const getCompanyData = async ( req,res) => {
+  try {
+
+  } catch(error) {
+
+  }
+}
+
+//post a new job
+export const postJob = async ( req,res) => {
+  try {
+
+  } catch(error) {
+
+  }
+}
+
+//Get company jobs Applicants
+export const getCompanyJobsApplicants = async ( req,res) => {
+  try {
+
+  } catch(error) {
+
+  }
+}
+
+//get company posted jobs
+export const getCompanyPostedJobs = async ( req,res) => {
+  try {
+
+  } catch(error) {
+
+  }
+}
+
+//Change job application stattus
+export const changeJobApplicationStatus = async ( req,res) => {
+  try {
+
+  } catch(error) {
+
+  }
+}
+
+// change job visibility
+export const changeJobVisibility = async ( req,res) => {
+  try {
+
+  } catch(error) {
+
+  }
+}
+
+
+
+
+
