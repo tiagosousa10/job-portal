fdf934b52e6c57da1f9870339279654ca93c9384
tiagosousa <tiagosousa.tams@hotmail.com>
2025-06-12 20:28:26 +0100

    protectCompany middleware created

---
+++ server/middlewares/authMiddleware.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken';
+import Company from '../models/Company.js';
+
+export const protectCompany = async (req,res, next) => {
+  const token = req.headers.token; // Get token from headers
+
+  if(!token) {
+    return res.json({success: false, message: 'No token, authorization denied' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET); // Verify token
+
+    req.company = await Company.findById(decoded.id).select('-password'); // Find user by ID
+
+    next(); // If valid token, continue to next middleware
+  } catch(error) {
+    res.json({success:false, message: 'Token is not valid - ' + error.message });
+  }
+}
