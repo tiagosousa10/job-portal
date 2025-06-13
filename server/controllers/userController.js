8f19825366438017d0b1d806dd3026781abb88d3
tiagosousa <tiagosousa.tams@hotmail.com>
2025-06-13 21:09:45 +0100

    apply for a job

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,6 @@
-import User from "../models/User";
+import Job from "../models/Job.js";
+import JobApplication from "../models/JobApplication.js";
+import User from "../models/User.js";
 
 //get user data
 export const getUserData = async (req, res) => {
@@ -17,7 +19,40 @@ export const getUserData = async (req, res) => {
 };
 
 //Apply for  a job
-export const applyForJob = async (req, res) => {};
+export const applyForJob = async (req, res) => {
+  const { jobId } = req.body;
+  const userId = req.auth.userId;
+
+  try {
+    const isAlreadyApplied = await JobApplication.find({ jobId, userId });
+
+    if (isAlreadyApplied.length > 0) {
+      return res.json({
+        success: false,
+        message: "You have already applied for this job",
+      });
+    }
+    const jobData = await Job.findById(jobId);
+
+    if (!jobData) {
+      return res.json({
+        success: false,
+        message: "Job not found",
+      });
+    }
+
+    await JobApplication.create({
+      companyId: jobData.companyId,
+      userId,
+      jobId,
+      date: Date.now(),
+    });
+
+    res.json({ success: true, message: "Job applied successfully" });
+  } catch (error) {
+    res.json({ success: false, message: "Error applying for job" });
+  }
+};
 
 //get user applied applications
 export const getUserJobApplications = async (req, res) => {};
