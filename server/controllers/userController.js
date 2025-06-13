5a2c914044b9501eac70ddf161a97b41ca717db8
tiagosousa <tiagosousa.tams@hotmail.com>
2025-06-13 21:03:10 +0100

    get user data

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,20 @@
+import User from "../models/User";
+
 //get user data
-export const getUserData = async (req, res) => {};
+export const getUserData = async (req, res) => {
+  const userId = req.auth.userId; // from clerk middleware
+  try {
+    const user = await User.findById(userId);
+
+    if (!user) {
+      res.json({ success: false, message: "User not found" });
+    }
+
+    res.json({ success: true, user });
+  } catch (error) {
+    res.json({ success: false, message: "Error getting user data" });
+  }
+};
 
 //Apply for  a job
 export const applyForJob = async (req, res) => {};
